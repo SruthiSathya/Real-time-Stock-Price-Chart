9f5027ba21852ff1a796989fb991d12e400dbfaa
Sruthi <sruthi921.ani@gmail.com>
2021-05-20 11:35:52 -0700

    New API added

--- index.js
+++ index.js
@@ -1,62 +1,51 @@
-                var RESULT = { 'Stock': 'Closing Price Value'}
+                var RESULT = {}
                 
                 function getSymbol(){  
                     var stock_symbol=document.getElementById("fname").value;  
-                    myFunction(stock_symbol) 
+                    setInterval(() => {updateStock(stock_symbol)}, 5000);
                 }  
 
-                function myFunction(stock_symbol) {
-                    setInterval(() => {updateStock(stock_symbol)}, 5000);
-                }
-
-                function updateStock(stock_symbol)
+                function updateStock(stock_symbol) 
                 {
-                    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+stock_symbol+"&interval=1min&apikey=KOE7PMEOOPD18BYD"
-                    stockChartYValuesFunction = [];
-                    
+                   url = "https://cloud.iexapis.com/stable/stock/"+stock_symbol+"/intraday-prices?token=sk_0ee1843b5d13482388927e848ff8b787&chartLast=20"
+      
+                  stock_values = [];
                     fetch(url)
                             .then(function(resp){
                                     return resp.json();
                             })
-                            .then(function(data){
-                                   // console.log(data)
-                                   stockChartYValuesFunction.push(data["Time Series (1min)"][  Object.keys(data["Time Series (1min)"])[0] ]["4. close"]);
-                                
-                                    var stock_val = parseFloat(stockChartYValuesFunction[0])
-
+                           .then(
+                            function (data)
+                            {
+                                    
+                                    idx_array = Object.keys(data)
+                                    console.log(   data[  [idx_array.length - 1]    ]["close"] );
+                                    var stock_val = data[  [idx_array.length - 1]    ]["close"]
                                     RESULT[String(stock_symbol)] = stock_val
-                                    
-                                    //console.log(RESULT)
-                                    
-                                    var arr = [];
+                                    var arr = [ ['Stock', 'Closing Price Value']  ];
                                     for (var key in RESULT) {
                                         if (RESULT.hasOwnProperty(key)) {
                                             arr.push( [ key, RESULT[key] ] );
                                         }
                                     }
-                                    //console.log(arr);
+                                    console.log(arr);
                                     drawMultSeries(arr) 
                                         }
-                            )};          
+                                    
+                           )};        
                                 google.charts.load('current', {packages: ['corechart', 'bar']});
                                 google.charts.setOnLoadCallback(drawMultSeries);
                                 
                                 function drawMultSeries(arr) {
-
                                     var data = google.visualization.arrayToDataTable(arr);
-                                    var options = {
-                                        title: 'Real Time Stock Price Chart',
-                                        chartArea: {width: '50%'},
+                                    var options = {title: 'Real Time Stock Price Chart', chartArea: {width: '50%'},
                                         hAxis: {
-                                        title: 'Closing Price',
+                                        title: 'Closing Price (in USD)',
                                         minValue: 0
                                         },
-                                        vAxis: {
-                                        title: 'Stock Symbol'
-                                        }
+                                        vAxis: {title: 'Stock Symbol'}
                                     };
 
                                     var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
                                     chart.draw(data, options);
-                                    }
-            
+                                    }                 
